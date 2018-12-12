c044fd4434574db0a7bd809af9193b561b1dc148
tuanhah <tuan.a.hoang.1998@gmail.com>
2018-12-12 22:30:20 +0700

    done

---
+++ index.js
@@ -0,0 +1,14 @@
+const fb = require("facebook-chat-api");
+const axios = require("axios");
+
+fb({email : "tuantsukuyomi@gmail.com",password:"tuanhoang"},(err,api)=>{
+    if (err) return console.error(err);
+    
+    api.listen((err, message) => {
+        axios.get('http://api.minhhieu.asia/vi.php',{params:{text:message.body}})
+        .then( response =>{
+          api.sendMessage(response.data, message.threadID);
+        } )
+  
+      });
+});
